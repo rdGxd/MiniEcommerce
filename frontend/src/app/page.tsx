0df3b3223f5377f24b8e10dd62411a89ea3ed45f
Rodrigo <rdg.hora1@gmail.com>
2025-09-17 16:07:48 -0300

    feat: adicionar configuração do Prettier e ESLint ao projeto; refatorar layout da página inicial

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,9 +7,12 @@ export default function Home() {
     <>
       {/* Texto da HOME */}
       <div className="flex flex-col items-center justify-center gap-6 p-4 text-center">
-        <h1 className="font-bold text-4xl">Find clothes that matches your style</h1>
+        <h1 className="font-bold text-4xl">
+          Find clothes that matches your style
+        </h1>
         <p className="text-center text-lg sm:text-2xl max-w-2xl">
-          Discover a wide range of clothing options tailored to your unique taste and preferences.
+          Discover a wide range of clothing options tailored to your unique
+          taste and preferences.
         </p>
         <Button size="lg" className="w-full cursor-pointer">
           Shop Now
@@ -41,15 +44,20 @@ export default function Home() {
             className="w-full h-auto object-cover rounded-lg"
           />
         </div>
-
-        {/* Logos das marcas  */}
-        <div className="grid grid-cols-3 gap-5 col-span-2 sm:col-span-1 mt-5 p-2 items-center dark:bg-gray-500 bg-black rounded-lg border border-gray-300">
-          <Image src={"/versace.svg"} alt="Versace" width={500} height={500} />
-          <Image src={"/zara-logo-1.svg"} alt="Zara" width={500} height={500} />
-          <Image src={"/gucci-logo-1.svg"} alt="Gucci" width={500} height={500} />
-          <Image src={"/prada-logo-1.svg"} alt="Prada" width={500} height={500} />
-          <Image src={"/Calvin.svg"} alt="Calvin Klein" width={500} height={500} className=" col-span-2 " />
-        </div>
+      </div>
+      {/* Logos das marcas  */}
+      <div className="grid grid-cols-3 gap-5 col-span-2 sm:col-span-1 p-2 justify-items-center items-center dark:bg-gray-500 bg-black rounded-lg border border-gray-300  h-full">
+        <Image src={"/versace.svg"} alt="Versace" width={116} height={23} />
+        <Image src={"/zara-logo-1.svg"} alt="Zara" width={63} height={26} />
+        <Image src={"/gucci-logo-1.svg"} alt="Gucci" width={109} height={25} />
+        <Image src={"/prada-logo-1.svg"} alt="Prada" width={127} height={21} />
+        <Image
+          src={"/Calvin.svg"}
+          alt="Calvin Klein"
+          width={134}
+          height={21}
+          className=" col-span-2"
+        />
       </div>
       <NewArrivals />
     </>
