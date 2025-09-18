c2531452420816b59314fad01bd867c43d2987ca
Rodrigo <rdg.hora1@gmail.com>
2025-09-18 11:16:55 -0300

    feat: atualizar componente Footer para usar ícones de redes sociais do pacote @icons-pack/react-simple-icons

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -1,31 +1,25 @@
-import { siFacebook, siGithub, siInstagram, siX } from "simple-icons";
+import {
+  SiFacebook,
+  SiGithub,
+  SiInstagram,
+  SiX,
+} from "@icons-pack/react-simple-icons";
 
 export function Footer() {
   return (
     <footer className="mt-20 flex w-full flex-col border-t bg-gray-200 px-4 py-10 text-sm">
-      <p className="text-left text-4xl font-extrabold">SHOP.CO</p>
+      <p className="text-left text-4xl font-extrabold dark:text-black">
+        SHOP.CO
+      </p>
       <p className="mt-2 max-w-2xl text-lg text-gray-600 sm:text-2xl">
         We have clothes that suits your style and which you’re proud to wear.
         From women to men.
       </p>
-      <div className="mt-6 flex gap-4 text-gray-600 [&_svg]:size-6">
-        <svg>
-          <title>X</title>
-          <path d={siX.path} />
-        </svg>
-        <svg>
-          <title>Facebook</title>
-          <path d={siFacebook.path} />
-        </svg>
-        <svg>
-          <title>Instagram</title>
-          <path d={siInstagram.path} />
-        </svg>
-
-        <svg>
-          <title>GitHub</title>
-          <path d={siGithub.path} />
-        </svg>
+      <div className="mt-6 flex gap-4 [&_svg]:size-7">
+        <SiX className="dark:border-dark cursor-pointer rounded border border-white bg-white p-1 hover:text-black dark:bg-black" />
+        <SiFacebook className="dark:border-dark cursor-pointer rounded border border-black bg-black p-1 text-white hover:text-gray-300 dark:border-white dark:bg-white dark:text-black" />
+        <SiInstagram className="dark:border-dark cursor-pointer rounded border border-white bg-white p-1 hover:text-black dark:bg-black" />
+        <SiGithub className="dark:border-dark cursor-pointer rounded border border-white bg-white p-1 hover:text-black dark:bg-black" />
       </div>
     </footer>
   );
