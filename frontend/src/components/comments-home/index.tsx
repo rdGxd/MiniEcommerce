593edc31165fd004fc5a751b578488a36d99608d
Rodrigo <rdg.hora1@gmail.com>
2025-09-19 09:26:00 -0300

    feat: adicionar componente Carousel para exibir comentários em formato de carrossel

--- frontend/src/components/comments-home/index.tsx
+++ frontend/src/components/comments-home/index.tsx
@@ -1,5 +1,12 @@
 import { getStarRating } from "@/helper/rating";
 import { ArrowLeftIcon, ArrowRightIcon, CircleCheckIcon } from "lucide-react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
 
 export function CommentsHome() {
   return (
@@ -15,28 +22,71 @@ export function CommentsHome() {
           <ArrowRightIcon />
         </div>
       </div>
-      <div className="m-2 flex flex-col rounded-2xl border border-gray-300 p-4 md:grid md:items-center md:-grid-cols-3">
-        <div>
-        {getStarRating(5)}
-        <p className="mt-4 flex gap-2 font-bold">
-          Sarah M. <CircleCheckIcon className="text-green-500" />
-        </p>
-        <p className="max-w-2xl text-center text-lg text-gray-400 sm:text-2xl">
-          "I recently purchased a dress from this online store, and I couldn't
-          be happier with my experience. The website was easy to navigate, and I
-          found the perfect dress for a special occasion. The quality of the
-          fabric and craftsmanship exceeded my expectations. I received numerous
-          compliments at the event, and I felt confident and beautiful wearing
-          it. The customer service was exceptional as well, with prompt
-          responses to my inquiries. I highly recommend this store to anyone
-          looking for stylish and high-quality clothing."
-        </p>
-        </div>
+      <div className="grid w-full max-w-5xl grid-cols-1 items-center justify-items-center p-4">
+        <Carousel
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+        >
+          <CarouselContent>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+              {getStarRating(5)}
+              <p className="mt-4 flex gap-2 font-bold">
+                Sarah M. <CircleCheckIcon className="text-green-500" />
+              </p>
+              <p className="max-w-2xl text-center text-lg text-gray-400 sm:text-2xl">
+                "I recently purchased a dress from this online store, and I
+                couldn't be happier with my experience. The website was easy to
+                navigate, and I found the perfect dress for a special occasion.
+                The quality of the fabric and craftsmanship exceeded my
+                expectations. I received numerous compliments at the event, and
+                I felt confident and beautiful wearing it. The customer service
+                was exceptional as well, with prompt responses to my inquiries.
+                I highly recommend this store to anyone looking for stylish and
+                high-quality clothing."
+              </p>
+            </CarouselItem>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+              {getStarRating(5)}
+              <p className="mt-4 flex gap-2 font-bold">
+                Sarah M. <CircleCheckIcon className="text-green-500" />
+              </p>
+              <p className="max-w-2xl text-center text-lg text-gray-400 sm:text-2xl">
+                "I recently purchased a dress from this online store, and I
+                couldn't be happier with my experience. The website was easy to
+                navigate, and I found the perfect dress for a special occasion.
+                The quality of the fabric and craftsmanship exceeded my
+                expectations. I received numerous compliments at the event, and
+                I felt confident and beautiful wearing it. The customer service
+                was exceptional as well, with prompt responses to my inquiries.
+                I highly recommend this store to anyone looking for stylish and
+                high-quality clothing."
+              </p>
+            </CarouselItem>
+            <CarouselItem className="md:basis-1/2 lg:basis-1/3">
+              {getStarRating(5)}
+              <p className="mt-4 flex gap-2 font-bold">
+                Sarah M. <CircleCheckIcon className="text-green-500" />
+              </p>
+              <p className="max-w-2xl text-center text-lg text-gray-400 sm:text-2xl">
+                "I recently purchased a dress from this online store, and I
+                couldn't be happier with my experience. The website was easy to
+                navigate, and I found the perfect dress for a special occasion.
+                The quality of the fabric and craftsmanship exceeded my
+                expectations. I received numerous compliments at the event, and
+                I felt confident and beautiful wearing it. The customer service
+                was exceptional as well, with prompt responses to my inquiries.
+                I highly recommend this store to anyone looking for stylish and
+                high-quality clothing."
+              </p>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
-      <div className="flex w-full items-center justify-center gap-2 p-4">
-        <span className="h-2 w-2 rounded-full bg-black"></span>
-        <span className="h-2 w-2 rounded-full bg-gray-300"></span>
-      </div>
+      <div className="flex w-full items-center justify-center gap-2 p-4"></div>
     </>
   );
 }
