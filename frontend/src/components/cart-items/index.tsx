77780852369614ce8a1c3425e46fbc341871a9e8
Rodrigo <rdg.hora1@gmail.com>
2025-10-07 09:41:10 -0300

    refactor: ajustar estilos e classes no componente CartItems
    Signed-off-by: Rodrigo <rdg.hora1@gmail.com>

--- frontend/src/components/cart-items/index.tsx
+++ frontend/src/components/cart-items/index.tsx
@@ -37,22 +37,27 @@ export function CartItems() {
   }
 
   return (
-    <div className="grid gap-6 border p-2 border-gray-200 rounded-2xl">
+    <div className="grid gap-6 rounded-2xl border border-gray-200 p-2">
       {MOCK_CART_ITEMS.map((item) => (
-        <div key={item.id} className="flex items-center gap-4 border-b pb-4 last:border-0 p-2 ">
+        <div
+          key={item.id}
+          className="flex items-center gap-4 border-b p-2 pb-4 last:border-0"
+        >
           <img
             src={item.imageURL}
             alt={item.name}
             className="h-36 w-24 object-cover"
           />
-          <div className="flex flex-1 flex-col justify-between h-full border-l pl-4  border-gray-200 border-dashed  ">
+          <div className="flex h-full flex-1 flex-col justify-between border-l border-dashed border-gray-200 pl-4">
             <h2 className="text-lg font-semibold">{item.name}</h2>
-            <p className="text-black font-bold text-xl">${item.price.toFixed(2)}</p>
+            <p className="text-xl font-bold text-black">
+              ${item.price.toFixed(2)}
+            </p>
 
             <div className="">
               <Button
                 onClick={() => handleRemoveItem(item.id)}
-                className="mt-2 rounded-full bg-transparent p-2 transition-colors hover:bg-red-100"
+                className="mt-2 cursor-pointer rounded-full bg-transparent p-2 transition-colors hover:bg-red-100"
               >
                 <Trash2Icon className="h-4 w-4 text-red-500" />
               </Button>
@@ -60,7 +65,7 @@ export function CartItems() {
                 <Button
                   onClick={() => handleAmountChange(item.id, item.quantity - 1)}
                   disabled={item.quantity <= 1}
-                  className="cursor-pointer border-none bg-transparent text-2xl text-black disabled:cursor-not-allowed disabled:opacity-50"
+                  className="cursor-pointer border-none bg-transparent text-2xl text-black hover:bg-transparent disabled:cursor-not-allowed disabled:opacity-50"
                 >
                   -
                 </Button>
@@ -68,7 +73,7 @@ export function CartItems() {
                 <p className="">{item.quantity}</p>
                 <Button
                   onClick={() => handleAmountChange(item.id, item.quantity + 1)}
-                  className="cursor-pointer border-none bg-transparent text-2xl text-black"
+                  className="cursor-pointer border-none bg-transparent text-2xl text-black hover:bg-transparent disabled:cursor-not-allowed disabled:opacity-50"
                 >
                   +
                 </Button>
